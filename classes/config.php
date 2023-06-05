030f1c1a99026a96395bbbbbdc36bd2fe9f4bb0c
thernloven <103603662+thernloven@users.noreply.github.com>
2023-06-05 12:29:28 -0300

    smtp settings

--- classes/config.php
+++ classes/config.php
@@ -4,14 +4,14 @@
 class Config {
 
 
-    const SMTP_HOST = 'sandbox.smtp.mailtrap.io';
+    const SMTP_HOST = 'smtp.gmail.com';
 
 
-    const SMTP_PORT = 2525;
+    const SMTP_PORT = 465;
 
-    const SMTP_USER = '9db87f77764fe8';
+    const SMTP_USER = 'noreply.xogos@gmail.com';
 
-    const SMTP_PASSWORD = 'e07a9dd28dadda';
+    const SMTP_PASSWORD = 'W@shingt0n';
 
 
 
