b104b0958001319ce69ecbd4c13378d5f57a3b05
thernloven <103603662+thernloven@users.noreply.github.com>
2023-06-12 07:25:51 -0300

    friends online

--- admin/includes/friends_online.php
+++ admin/includes/friends_online.php
@@ -36,25 +36,27 @@
 
                 <?php if(is_student()): ?>
 
-                  <?php 
-                 
-                        $query = "SELECT * FROM users_online WHERE {$_SESSION['student_id']} = parent_id";
-                        $select_students = mysqli_query($connection, $query);
-          
+                  <?php
+                    $query = "SELECT * FROM users_online WHERE (online_parent_id = ? AND online_teacher_id = ?) OR online_parent_id = ?";
+                    $stmt = mysqli_prepare($connection, $query);
+                    mysqli_stmt_bind_param($stmt, "iii", $_SESSION['student_id'], $_SESSION['t_student_id'], $_SESSION['student_id']);
+                    mysqli_stmt_execute($stmt);
+                    $result = mysqli_stmt_get_result($stmt);
 
-                          while ($row = mysqli_fetch_assoc($select_students)) {
-                          $online_id        = $row['online_id'];
-                          $online_firstname = $row['online_firstname'];
-                          $online_img       = $row['online_img'];
-                      
-                      echo "<div class='col-lg-4 col-md-4 col-sm-4 col-4 text-center'>
-                          <a href='./chat.php'>
-                          <img style='border-radius:100%; border: 2px solid #74FFBA; height:80px' src='assets/img/avatars/$online_img' alt=''>
-                          </a>
-                          <h5>$online_firstname</h5>
-                      </div>";
-                  }
-                  ?>
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $online_id = $row['online_id'];
+                        $online_firstname = $row['online_firstname'];
+                        $online_img = $row['online_img'];
+
+                        echo "<div class='col-lg-4 col-md-4 col-sm-4 col-4 text-center'>
+                                  <a href='./chat.php'>
+                                  <img style='border-radius:100%; border: 2px solid #74FFBA; height:80px' src='assets/img/avatars/$online_img' alt=''>
+                                  </a>
+                                  <h5>$online_firstname</h5>
+                              </div>";
+                    }
+                    ?>
+
 
 
                 <?php endif ?>
