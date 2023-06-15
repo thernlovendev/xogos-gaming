1c3312dbd9bf0f702313edc9980f5d07df6add16
thernloven <103603662+thernloven@users.noreply.github.com>
2023-06-15 18:32:16 -0300

    small updates

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -60,7 +60,6 @@ $success = "";
   <script src="assets/js/plugins/bootstrap-notify.js"></script>
   <!-- Control Center for Black Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="assets/js/scripts.min.js?v=1.0.0"></script><!-- Black Dashboard DEMO methods, don't include it in your project! -->
-  <script src="assets/demo/demo.js"></script>
 
   <!-- Initialize tooltips -->
 <script>
--- admin/index.php
+++ admin/index.php
@@ -2,20 +2,29 @@
 <?php include "includes/sidebar.php" ?>
 <?php include "includes/navbar.php" ?>
   
-    
       <!-- End Navbar -->
       <div class="content">
       <div class="row">
       <?php include "includes/coins.php" ?>
 
-      <?php include "includes/performance_old.php" ?>
+      <?php if(is_student() OR is_parent() OR is_teacher() ): ?>
+      <?php include "includes/performance.php" ?>
+      <?php endif ?>
+      <?php if(is_admin()): ?>
+      <?php include "includes/performance_admin.php" ?>
+      <?php endif ?>
       <div class="row">
       
       <?php include "includes/friends_online.php" ?>
 
       <?php include "includes/top_players.php" ?>
 
-      <?php include "includes/top_games.php" ?>        
+      <?php if(is_student() OR is_parent() OR is_teacher() ): ?>
+      <?php include "includes/top_games_admin.php" ?>
+      <?php endif ?>
+      <?php if(is_admin()): ?>
+      <?php include "includes/top_games.php" ?>
+      <?php endif ?>          
         </div>
       </div>
       <?php include "includes/footer.php" ?>
