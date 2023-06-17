24f27af91a652a8a953522a109015f47eb615068
thernloven <103603662+thernloven@users.noreply.github.com>
2023-06-17 00:23:25 -0300

    db change

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
 $db['db_host'] = 'localhost';
-$db['db_user'] = 'thernlov_admin';
-$db['db_pass'] = 'root1';
-$db['db_name'] = 'thernlov_xogos';
+$db['db_user'] = 'admin';
+$db['db_pass'] = '52a6d848b3a02dec4792ba937d3a98f810a5b446af4da5d1';
+$db['db_name'] = 'xogos';
 
 foreach ($db as $key => $value) {
 
@@ -13,4 +13,4 @@ $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 //if ($connection) {
   //   echo 'We are connected';
- //} 52a6d848b3a02dec4792ba937d3a98f810a5b446af4da5d1
+ //}
--- includes/logout.php
+++ includes/logout.php
@@ -7,7 +7,7 @@ $user_id = session_id();
 // Include your database connection
 // Replace 'your_host', 'your_username', 'your_password', and 'your_database' with your actual database credentials
 
-$connection = mysqli_connect('localhost', 'thernlov_admin', 'root1', 'thernlov_xogos');
+$connection = mysqli_connect('localhost', 'admin', '52a6d848b3a02dec4792ba937d3a98f810a5b446af4da5d1', 'xogos');
 if (!$connection) {
     die('Database connection failed: ' . mysqli_connect_error());
 }
