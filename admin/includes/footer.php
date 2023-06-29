3d84eb402a613d036f5c5ef85e447f5eaed0b58e
thernloven <103603662+thernloven@users.noreply.github.com>
2023-06-29 00:16:54 -0300

    success modal register kids

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -60,6 +60,7 @@ $success = "";
   <script src="assets/js/plugins/bootstrap-notify.js"></script>
   <!-- Control Center for Black Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="assets/js/scripts.min.js?v=1.0.0"></script><!-- Black Dashboard DEMO methods, don't include it in your project! -->
+  <script src="assets/demo/demo.js"></script>
  
   <?php if ($success) { ?>
   <script>
